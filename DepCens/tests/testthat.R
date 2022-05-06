ef87c2b4bd2dfa6e701b2183f16d581d73479e7d
GabrielGrandemagne <ggrandemagne.santos@gmail.com>
2022-05-06 17:18:07 -0300

    Removing duplicates

--- DepCens/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(DepCens)
-
-test_check("DepCens")
--- DepCens/tests/testthat/test_dependentcensor.R
+++
@@ -1,9 +0,0 @@
-library(DepCens)
-
-# MEP Approach
-fit <- dependent.censoring(formula = time ~ x1 | x1, data=KidneyMimic, delta_t=KidneyMimic$delta_t,
-                           delta_c=KidneyMimic$delta_c, ident=KidneyMimic$ident, dist = "mep")
-
-# Weibull Approach
-fit <- dependent.censoring(formula = time ~ x1 + x2 | x1 + x2, data=KidneyMimic, delta_t=KidneyMimic$delta_t,
-                           delta_c=KidneyMimic$delta_c, ident=KidneyMimic$ident, dist = "weibull")
